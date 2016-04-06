8a87d15fa9233ac6c0112b2d0433eff03694469c
Vibha <vibha.sazawal@gmail.com>
2016-04-06 14:29:44 -0400

    model object for trains

---
+++ app/src/main/java/edu/umd/cs/sodorfriends/Train.java
@@ -0,0 +1,23 @@
+package edu.umd.cs.sodorfriends;
+
+/**
+ * Created by Vibha on 4/6/2016.
+ */
+public class Train {
+
+    private String name;
+    private int number;
+
+    public Train(String s, int i) {
+        name = s;
+        number = i;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getNumber() {
+        return number;
+    }
+
+}
